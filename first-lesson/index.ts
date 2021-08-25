d914456e36bb3e856e4e4370953a0bf94508e5f3
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-08-25 23:29:17 +0900

    Add

--- first-lesson/index.ts
+++ first-lesson/index.ts
@@ -4,3 +4,59 @@ console.log(hello);
 function add(a: number, b: number): number {
   return a + b;
 }
+
+let hasValue: boolean = true;
+
+// Typescriptでは整数も浮動小数点もマイナスもnumberで表すことができる
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+
+
+
+/*
+型推論の場合
+
+let hasValue = true;
+let count = 10;
+let float = 3.14;
+let negative = -0.12;
+
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
+let hello;
+*/
+
+
+// any型
+const fruits: string[] = ['Apple', 'Banana', 'Grape']
+const fruit = fruits[0];   // -> fruit: string
+
+
+// Tuple型 [決まった型の配列にしたい！！]
+const book: [string, number, boolean] = ['business', 1500, false];
+
+book[1] = 700  // -> OK!
+
+
+// Enum型
+enum CoffeeSize {
+  SHORT = 'SHORT',
+  TALL = 'TALL',
+  GRANDE = 'GRANDE',
+  VENTI = 'VENTI'
+}
+ 
+const coffee = {
+  host: true,
+  size: CoffeeSize.TALL
+}
+
+ // Union型
+let unionType: number | string = 10
+let unionTypes: (number | string)[] = [21, 'hello']
