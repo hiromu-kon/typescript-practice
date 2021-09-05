aba89b5ed6b1e4f2265c8a4d03721f1759562961
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-09-05 19:47:22 +0900

    Fix

---
+++ ts-basic/src/basic/any.ts
@@ -0,0 +1,8 @@
+
+export default function anySample() {
+  let name: any = 'Hello' // string型を代入
+  console.log('any sample 1:', typeof name, name)
+
+  name = 28 // number型を再代入できる
+  console.log('any sample 2:', typeof name, name)
+}
---
+++ ts-basic/src/index.ts
@@ -0,0 +1,26 @@
+// import World from './world'
+
+// const root: HTMLElement | null = document.getElementById('root')
+// const world = new World('Hello World!!!')
+// world.sayHello(root)
+
+// 03. 基本の型定義
+
+import { primitiveSample, notExist, anySample, unknownSample } from "./basic";
+
+primitiveSample()
+notExist()
+anySample()
+unknownSample()
+
+// 04.関数の型定義
+import { logMessage, logMessage5 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript!')
+logMessage5('Hello Type Signature!')
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('123')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(1000, 2000, 500, 3000, 250)
+console.log('Function parameters sample 5: Total price:', sum)
