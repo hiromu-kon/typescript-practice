1fa20f7096b478fc21bc99aac1b099ee68b07a23
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-09-22 23:59:07 +0900

    fix

---
+++ daily-food-app/src/food-app.ts
@@ -0,0 +1,14 @@
+class Score {}
+class Food {
+  
+}
+
+class Foods {
+
+  elements = document.querySelectorAll('.food');
+  constructor() {
+    this.elements.forEach(element => {
+    })
+  }
+}
+const foods = new Foods();
