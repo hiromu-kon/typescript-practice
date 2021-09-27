c9bb2faf1be415de460b463df597ef454016ef22
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-09-27 22:45:56 +0900

    fix

--- first-lesson/src/interface.ts
+++ first-lesson/src/interface.ts
@@ -31,3 +31,46 @@ const tmpDeveloper = {
 const user: Human = tmpDeveloper;
 let developer = new Developer('Quill', 39, 3);
 developer.name = 'John';
+
+type Nameable = {
+	name: string;
+}
+
+interface Human extends Nameable {
+	age: number;
+	greeting(message: string): void;
+}
+
+// typeエイリアスの場合
+type addFunc = (num1: number, num2: number) => number;
+
+// interfaceの場合
+// オブジェクトと混同しやすいため基本的にはtypeを使う
+interface addFunc {
+	(num1: number, num2: number): number;
+}
+
+let addFunc: addFunc;
+addFunc = (n1: number, n2:number) => {
+	return n1 + n2;
+}
+
+type Nameable = {
+	name: string;
+	nickName?: string;
+}
+
+const nameable: Nameable = {
+	name: 'Quill',
+	nickName: 'Quilla'
+}
+
+// メソッドにもつけることができる
+interface Human extends Nameable {
+	age: number;
+	greeting?(message: string): void;
+}
+
+class Developer implements Human {
+	constructor(public name: string, public age: number) {}
+}
