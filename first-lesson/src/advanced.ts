29f80accdb22154267c8db093c1d42431e512af9
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-10-28 23:22:14 +0900

    fix

--- first-lesson/src/advanced.ts
+++ first-lesson/src/advanced.ts
@@ -1,169 +1,94 @@
-type Engineer = {
-	name: string;
-	role: string;
-}
-
-type Blogger = {
-	name: string;
-	follower: number;
-}
-
-type EngineerBlogger = Engineer & Blogger;
-
-const quill: EngineerBlogger = {
-	name: '',
-	role: 'front-end',
-	follower: 1000
-}
-
-/* interfaceの場合 */
-
 interface Engineer {
 	name: string;
 	role: string;
-}
-
-interface Blogger {
+  }
+  interface Blogger {
 	name: string;
 	follower: number;
-}
-
-interface EngineerBlogger extends Engineer, Blogger { }
-
-
-/* union型で使用 */
-
-type NumberBoolean = number | boolean;
-type StringNumber = string | number;
-type Mix = NumberBoolean & StringNumber   // -> type Mix = number
-
-/* パターン① */
-function toUpperCase(x: string | number) {
-	if (typeof x === 'string') {
-		return x.toUpperCase();    // 条件文を使わないとtoUpperCaseは使えずstringとnumberで使えるメソッドしか使えない
-	}
-	return '';
-}
-
-/* パターン② */
-type NomadWorker = Engineer | Blogger;
-function describeProfile(nomadWorker: NomadWorker) {
-	console.log(nomadWorker.name);  　　　// 上記の通りnameしか使えない
-
-	/* if (typeof nomadWorker === 'object') typeofだとnomadWorkerがオブジェクトのためobjectしか使えない */
-
-	if ('role' in nomadWorker) {
-		console.log(nomadWorker.role);
-	}
-}
-
-
-/* パターン③ */
-class Dog {
-	speak() {
-		console.log('bow-wow');
-	}
-}
-
-class Bird {
-	speak() {
-		console.log('tweet-tweet');
-	}
-	fly() {
-		console.log('flutter');
-	}
-}
-
-type Pet = Dog | Bird;
-
-function havePet(pet: Pet) {
-	pet.speak();
-
-	if (pet instanceof Bird) {
-		pet.fly();
-	}
-}
-
-havePet(new Bird());
-havePet(new Dog());
-
-class Dog {
-	kind: 'dog' = 'dog'
-	speak() {
-		console.log('bow-wow');
-	}
-}
-
-class Bird {
-	kind: 'bird' = 'bird'
-	speak() {
-		console.log('tweet-tweet');
-	}
-	fly() {
-		console.log('flutter');
-	}
-}
-
-type Pet = Dog | Bird;
-
-function havePet(pet: Pet) {
-	pet.speak();
-	switch (pet.kind) {
-		case 'bird':
-			pet.fly();
-	}
-}
-
-const input = document.getElementById('input');  // const input: HTMLElement | null
-
-input.value  // HTMLElementだとvalueが使えないためエラーになる
-
-
-// 下二つは同じものになるがJSXなどを使用している場合、タグとの区別が難しくなるためasの方を使う
-const input = <HTMLInputElement>document.getElementById('input');
-const input = document.getElementById('input') as HTMLInputElement;
-
-const input = document.getElementById('input');  
-// const input: HTMLElement | null
-
-const input = document.getElementById('input')!;  
-// const input: HTMLElement
-
-
-// 通常の場合
-interface Designer {
-	name: string;
-}
-
-const designer: Designer = {
-	name: 'Quill'
-}
-
-// designer.role = 'web'; 通常はメンバを追加することはできない
-
-
-/* インデックスシグネチャの場合
-   インデスクシグネチャを使用する場合はインデックスシグネチャの型に合わせないとエラーになる */
-
-interface Designer {
-	name: string;           // インデックスシグネチャを使用している場合、「age: number」などにすることはできない
-	[index: string]: string;
-}
-
-const designer: Designer = {
+  }
+  // type EngineerBlogger = Engineer & Blogger;
+  interface EngineerBlogger extends Engineer, Blogger { }
+  
+  const quill: EngineerBlogger = {
 	name: 'Quill',
-	role: 'web'
-}
-
-
-function toUpperCase(x: string): string;
-// function toUpperCase(x: number): number;  オーバーロードは複数作ることができる
-
-function toUpperCase(x: string | number): {
+	role: 'front-end',
+	follower: 1000
+  }
+  type NumberBoolean = number | boolean;
+  type StringNumber = string | number;
+  type Mix = NumberBoolean & StringNumber;
+  
+  function toUpperCase(x: string): string;
+  function toUpperCase(x: number): number;
+  function toUpperCase(x: string | number): string | number {
 	if (typeof x === 'string') {
-		return x.toUpperCase();
+	  return x.toUpperCase();
 	}
 	return x;
-}
-
-const upperHello = toUpperCase('hello');
+  }
+  const upperHello = toUpperCase('hello');
+  
+  type NomadWorker = Engineer | Blogger;
+  function describeProfile(nomadWorker: NomadWorker) {
+	console.log(nomadWorker.name);
+	if ('role' in nomadWorker) {
+	  console.log(nomadWorker.role);
+	}
+	if ('follower' in nomadWorker) {
+	  console.log(nomadWorker.follower);
+	}
+  }
+  class Dog {
+	kind: 'dog' = 'dog'
+	speak() {
+	  console.log('bow-wow');
+	}
+  }
+  class Bird {
+	kind: 'bird' = 'bird';
+	speak() {
+	  console.log('tweet-tweet');
+	}
+	fly() {
+	  console.log('flutter');
+	}
+  }
+  type Pet = Dog | Bird;
+  function havePet(pet: Pet) {
+	pet.speak();
+	switch (pet.kind) {
+	  case 'bird':
+		pet.fly();
+	}
+	if (pet instanceof Bird) {
+	  pet.fly();
+	}
+  }
+  havePet(new Bird());
+  
+  const input = document.getElementById('input') as HTMLInputElement;
+  input.value = 'initial input value';
+  (<HTMLInputElement>document.getElementById('input')).value = 'initial input value';
+  
+  interface Designer {
+	name: string;
+	[index: string]: string;
+  }
+  const designer: Designer = {
+	name: 'Quill',
+	role: 'web'
+  }
+  interface DownloadedData {
+	id: number;
+	user?: {
+	  name?: {
+		first: string;
+		last: string;
+	  }
+	}
+  }
+  const downloadedData: DownloadedData = {
+	id: 1
+  }
+  console.log(downloadedData.user?.name?.first);
+  const userData = downloadedData.user ?? 'no-user';
